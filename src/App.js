e43590293107fd30a7f40f57591d9cf4c341ff46
Nguyen Huynh Nhu <huynhnhu753nfs@gmail.com>
2024-02-02 21:27:58 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React day 2/2
         </a>
       </header>
     </div>
